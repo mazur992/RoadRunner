04fcc40ba67336eb41f7f0425512437c0f6f66e0
Mazur Roman <superioritywork@gmail.com>
2023-09-16 01:33:12 +0200

    add favicon, home img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import { HomeStyle } from './Home.styled';
 
 export default function Home() {
-  return <HomeStyle>Home</HomeStyle>;
+  return (
+    <HomeStyle>
+      <div className="companyName">RoadRunner</div>
+    </HomeStyle>
+  );
 }
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const HomeStyle = styled.div``;
+export const HomeStyle = styled.div`
+  position: relative;
+  height: 95vh;
+  background-image: url('https://cdn.pixabay.com/photo/2017/05/02/15/30/streets-2278471_1280.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  color: rgba(255, 255, 255, 0.8);
+  & .companyName {
+    font-size: 180px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
